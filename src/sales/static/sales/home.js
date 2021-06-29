e371b2f6e8431398c7223cbf4e5d9d43e9ad8c6b
Igor Slazyk <islazykv@gmail.com>
2021-06-29 17:00:43 +0200

    added report object

--- src/sales/static/sales/home.js
+++ src/sales/static/sales/home.js
@@ -4,6 +4,15 @@ const reportBtn = document.getElementById('report-btn')
 const img = document.getElementById('img')
 const modalBody = document.getElementById('modal-body')
 
+const reportForm = document.getElementById('report-form')
+
+const reportName = document.getElementById('id_name')
+const reportRemarks = document.getElementById('id_remarks')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
+
+console.log(reportName)
+console.log(reportRemarks)
+
 if (img){
     reportBtn.classList.remove('not-visible')
 }
@@ -11,4 +20,31 @@ if (img){
 reportBtn.addEventListener('click', ()=>{
     img.setAttribute('class', 'w-100')
     modalBody.prepend(img)
+
+    console.log(img.src)
+
+    reportForm.addEventListener('submit', e=>{
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('csrfmiddlewaretoken', csrf)
+        formData.append('name', reportName.value)
+        formData.append('remarks', reportRemarks.value)
+        formData.append('image', img.src)
+
+        $.ajax({
+            type: 'POST',
+            url: '/reports/save/',
+            data: formData,
+            success: function(response){
+                console.log(response)
+            },
+            error: function(error){
+                console.log(error)
+            },
+            processData: false,
+            contentType: false
+        })
+    
+
+    })
 })
